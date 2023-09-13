bf45db4d6197325347a8d445a65213faad820037
MihPar <9496260@gmail.com>
2023-09-13 10:27:07 +0300

    start_project13.10.27

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,13 @@
 //     console.log(`Example app listening on port ${port}`)
 // })
 
-import express from 'express'
+import express, {Request, Response} from 'express'
 const app = express()
 const PORT = process.env.PORT || 4000
 
 app.use(express.json())
 
-app.get('/', function(req, res) {
+app.get('/', function(req: Request, res: Response) {
 	res.json('Hello samuray')
 })
 
