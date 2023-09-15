26d45e432b19ecbcbd8b3f8d30f2db9636337c85
MihPar <9496260@gmail.com>
2023-09-15 12:43:52 +0300

    start-project15.12.43

--- dist/index.js
+++ dist/index.js
@@ -15,54 +15,67 @@ const db_1 = require("./db/db");
 const utils_1 = require("./utils");
 const PORT = process.env.PORT || 4000;
 exports.app.use(express_1.default.json());
+let objError = {
+    errorsMessages: [
+        {
+            message: "incorect iput value",
+            field: "author"
+        }
+    ]
+};
+/****************************** DELETE **************************************/
+exports.app.delete('/', function (req, res) {
+    let result = db_1.videos.splice(0, db_1.videos.length - 1);
+    res.sendStatus(utils_1.HTTP_STATUS.NO_CONTENT_204);
+});
 /******************************* GET ****************************************/
-exports.app.get('/videos', function (req, res) {
+exports.app.get('/', function (req, res) {
     res.json(db_1.videos);
 });
 /******************************* POST ****************************************/
 exports.app.post('/videos', function (req, res) {
-    if (!req.body.title || typeof req.body.title !== 'string' || req.body.title.length === 0) {
-        let objError = {
+    const titleList = req.body.title;
+    if (!titleList || typeof titleList !== 'string' || titleList.length === 0 || !titleList.trim()) {
+        res.status(utils_1.HTTP_STATUS.BAD_REQUEST_400).json({
             errorsMessages: [
                 {
                     message: "incorect iput value",
                     field: "title"
                 }
             ]
-        };
-        res.status(utils_1.HTTP_STATUS.BAD_REQUEST_400).json(objError);
+        });
         return;
     }
-    if (!req.body.author || typeof req.body.author !== 'string' || req.body.author.length === 0) {
-        let objError = {
+    const authorList = req.body.author;
+    if (!authorList || typeof authorList !== 'string' || authorList.length === 0 || !authorList.trim()) {
+        res.status(utils_1.HTTP_STATUS.BAD_REQUEST_400).json({
             errorsMessages: [
                 {
                     message: "incorect iput value",
                     field: "author"
                 }
             ]
-        };
-        res.status(utils_1.HTTP_STATUS.BAD_REQUEST_400).json(objError);
+        });
         return;
     }
-    if (Array.isArray(req.body.availableResolutions) && req.body.availableResolutions.length !== 0) {
-        let objError = {
-            errorsMessages: [
-                {
-                    message: "incorect iput value",
-                    field: "availableResolutions"
-                }
-            ]
-        };
+    const avalilabeRes = req.body.availableResolutions;
+    if (Array.isArray(avalilabeRes) && avalilabeRes.length !== 0) {
         const availableResolutions = ['P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160'];
-        for (let i = 0; i < req.body.availableResolutions.length; i++) {
-            if (!availableResolutions.includes(req.body.availableResolutions[i])) {
-                res.status(utils_1.HTTP_STATUS.BAD_REQUEST_400).json(objError);
+        for (let i = 0; i < avalilabeRes.length; i++) {
+            if (!availableResolutions.includes(avalilabeRes[i])) {
+                res.status(utils_1.HTTP_STATUS.BAD_REQUEST_400).json({
+                    errorsMessages: [
+                        {
+                            message: "incorect iput value",
+                            field: "titavailableResolutionsle"
+                        }
+                    ]
+                });
                 return;
             }
         }
     }
-    if (req.body.title.length <= 40 && req.body.author.length <= 20) {
+    if (titleList.length <= 40 && authorList.length <= 20) {
         const newVideos = {
             id: Number(new Date()),
             title: req.body.title,
@@ -91,32 +104,90 @@ exports.app.get('/videos/:id/', function (req, res) {
 });
 /******************************* PUT ****************************************/
 exports.app.put('/videos/:id/', function (req, res) {
-    // if(!req.body.title) {
-    // 	let result = {
-    // 		errorsMessages: [
-    // 		  {
-    // 			message: 'If the inputModel has incorrect values',
-    // 			field: "string"
-    // 		  }
-    // 		]
-    // 	  }
-    // 	res.sendStatus(HTTP_STATUS.NOT_FOUND_404)
-    // 	res.json(result.errorsMessages[0].message)
-    // 	return
-    // }
+    if (!req.body.title || typeof req.body.title !== 'string' || req.body.title === 0 || !req.body.title.trim()) {
+        res.status(utils_1.HTTP_STATUS.BAD_REQUEST_400).json({
+            errorsMessages: [
+                {
+                    message: "incorect iput value",
+                    field: "title"
+                }
+            ]
+        });
+        return;
+    }
+    if (!req.body.author || typeof req.body.author !== 'string' || req.body.author.length !== 0 || !req.body.author.trim()) {
+        res.status(utils_1.HTTP_STATUS.BAD_REQUEST_400).json({
+            errorsMessages: [
+                {
+                    message: "incorect iput value",
+                    field: "author"
+                }
+            ]
+        });
+        return;
+    }
+    if (Array.isArray(req.body.availableResolutions) && req.body.availableResolutions !== 0) {
+        const availableResolutions = ['P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160'];
+        for (let i = 0; i < req.body.availableResolutions; i++) {
+            if (!availableResolutions.includes(req.body.availableResolutions[i])) {
+                res.status(utils_1.HTTP_STATUS.BAD_REQUEST_400).json({
+                    errorsMessages: [
+                        {
+                            message: "incorect iput value",
+                            field: "availableResolutions"
+                        }
+                    ]
+                });
+                return;
+            }
+        }
+    }
+    if (!req.body.canBeDownloaded || typeof req.body.canBeDownloaded !== 'boolean') {
+        res.status(utils_1.HTTP_STATUS.BAD_REQUEST_400).json({
+            errorsMessages: [
+                {
+                    message: "incorect iput value",
+                    field: "canBeDownloaded"
+                }
+            ]
+        });
+        return;
+    }
+    if (!req.body.minAgeRestriction || typeof req.body.minAgeRestriction !== 'number' || req.body.minAgeRestriction !== 18) {
+        res.status(utils_1.HTTP_STATUS.BAD_REQUEST_400).json({
+            errorsMessages: [
+                {
+                    message: "incorect iput value",
+                    field: "minAgeRestriction"
+                }
+            ]
+        });
+        return;
+    }
+    if (!req.body.publicationDate || typeof req.body.publicationDate !== 'string' || req.body.publicationDate.length === 0 || req.body.publicationDate !== "2023-09-15T06:42:08.275Z" || !req.body.publicationDate.trim()) {
+        res.status(utils_1.HTTP_STATUS.BAD_REQUEST_400).json({
+            errorsMessages: [
+                {
+                    message: "incorect iput value",
+                    field: "publicationDate"
+                }
+            ]
+        });
+        return;
+    }
     let foundVideos = db_1.videos.find(function (v) {
         return v.id === Number(req.params.id);
     });
-    // if(!foundVideos) {
-    // 	res.sendStatus(HTTP_STATUS.BAD_REQUEST_400)
-    // 	return
-    // }
+    if (!foundVideos) {
+        res.status(utils_1.HTTP_STATUS.NOT_FOUND_404);
+        return;
+    }
     foundVideos.title = req.body.title;
     foundVideos.author = req.body.author;
     foundVideos.canBeDownloaded = req.body.canBeDownloaded;
     foundVideos.minAgeRestriction = req.body.minAgeRestriction;
     foundVideos.publicationDate = req.body.publicationDate;
-    res.sendStatus(utils_1.HTTP_STATUS.NO_CONTENT_204);
+    res.status(utils_1.HTTP_STATUS.NO_CONTENT_204);
 });
 /******************************* DELETE{id} ****************************************/
 exports.app.delete('/videos/:id/', function (req, res) {
--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,32 @@ const PORT = process.env.PORT || 4000
 
 app.use(express.json())
 
+type newObj = {
+	message: string,
+	field: string
+}
+type obj = {
+	// errorsMessages: Array<newObj>
+	errorsMessages: newObj[]
+}
+let objError: obj = {
+	errorsMessages: [
+	  {
+		message: "incorect iput value",
+		field: "author"
+	  }
+	]
+  }
+
+/****************************** DELETE **************************************/
+
+app.delete('/', function(req: Request, res: Response) {
+	let result = videos.splice(0, videos.length - 1)
+	res.sendStatus(HTTP_STATUS.NO_CONTENT_204)
+})
+
 /******************************* GET ****************************************/
-app.get('/videos', function(req: Request, res: Response) {
+app.get('/', function(req: Request, res: Response) {
 	res.json(videos)
 })
 
@@ -34,52 +58,55 @@ app.post('/videos', function(req: Request, res: Response) {
 		publicationDate: string;
 		availableResolutions: Array<string>;
 	  };
-
 	  
-	  
-	if(!req.body.title || typeof req.body.title !== 'string' || req.body.title.length === 0) {
-		let objError  = {
+	const titleList: string = req.body.title
+	if(!titleList || typeof titleList !== 'string' || titleList.length === 0 || !titleList.trim()) {
+		
+		res.status(HTTP_STATUS.BAD_REQUEST_400).json({
 			errorsMessages: [
 			  {
 				message: "incorect iput value",
 				field: "title"
 			  }
 			]
-		  }
-		res.status(HTTP_STATUS.BAD_REQUEST_400).json(objError)
+		  })
 		return 
 	}
-	if(!req.body.author || typeof req.body.author !== 'string' || req.body.author.length === 0) {
-		let objError  = {
+
+	const authorList: string = req.body.author
+	if(!authorList || typeof authorList !== 'string' || authorList.length === 0 || !authorList.trim()) {
+		
+		res.status(HTTP_STATUS.BAD_REQUEST_400).json({
 			errorsMessages: [
 			  {
 				message: "incorect iput value",
 				field: "author"
 			  }
 			]
-		  }
-		res.status(HTTP_STATUS.BAD_REQUEST_400).json(objError)
+		  })
 		return 
 	}
-	if(Array.isArray(req.body.availableResolutions) && req.body.availableResolutions.length !==0) {
-		let objError  = {
-			errorsMessages: [
-			  {
-				message: "incorect iput value",
-				field: "availableResolutions"
-			  }
-			]
-		  }
+
+	const avalilabeRes: Array<string> = req.body.availableResolutions
+	if(Array.isArray(avalilabeRes) && avalilabeRes.length !== 0) {
+		
 		const availableResolutions  = [ 'P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160' ]
-		for(let i = 0; i < req.body.availableResolutions.length; i++) {
-			if(!availableResolutions.includes(req.body.availableResolutions[i])) {
-				res.status(HTTP_STATUS.BAD_REQUEST_400).json(objError)
+		for(let i = 0; i < avalilabeRes.length; i++) {
+			if(!availableResolutions.includes(avalilabeRes[i])) {
+				res.status(HTTP_STATUS.BAD_REQUEST_400).json({
+					errorsMessages: [
+					  {
+						message: "incorect iput value",
+						field: "titavailableResolutionsle"
+					  }
+					]
+				  })
 				return 
 			}
 		}
 	}
 	
-	  if(req.body.title.length <= 40 && req.body.author.length <= 20) {
+	  if(titleList.length <= 40 && authorList.length <= 20) {
 		const newVideos: newVideosType = {
 			id: Number(new Date()),
 			title: req.body.title,
@@ -112,27 +139,94 @@ app.get('/videos/:id/', function(req: Request, res: Response) {
 /******************************* PUT ****************************************/
 
 app.put('/videos/:id/', function(req: Request, res: Response) {
-	// if(!req.body.title) {
-	// 	let result = {
-	// 		errorsMessages: [
-	// 		  {
-	// 			message: 'If the inputModel has incorrect values',
-	// 			field: "string"
-	// 		  }
-	// 		]
-	// 	  }
-	// 	res.sendStatus(HTTP_STATUS.NOT_FOUND_404)
-	// 	res.json(result.errorsMessages[0].message)
-	// 	return
-	// }
+	if(!req.body.title || typeof req.body.title !== 'string' || req.body.title === 0 || !req.body.title.trim()) {
+		
+		res.status(HTTP_STATUS.BAD_REQUEST_400).json({
+			errorsMessages: [
+			  {
+				message: "incorect iput value",
+				field: "title"
+			  }
+			]
+		  })
+		return 
+	}
+	
+	
+	if(!req.body.author || typeof req.body.author !== 'string' || req.body.author.length !== 0 || !req.body.author.trim()) {
+		
+		res.status(HTTP_STATUS.BAD_REQUEST_400).json({
+			errorsMessages: [
+			  {
+				message: "incorect iput value",
+				field: "author"
+			  }
+			]
+		  })
+		return 
+	}
+
+	if(Array.isArray(req.body.availableResolutions) && req.body.availableResolutions !== 0) {
+		const availableResolutions  = [ 'P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160' ]
+		for(let i = 0; i < req.body.availableResolutions; i++) {
+			if(!availableResolutions.includes(req.body.availableResolutions[i])) {
+				res.status(HTTP_STATUS.BAD_REQUEST_400).json({
+					errorsMessages: [
+					  {
+						message: "incorect iput value",
+						field: "availableResolutions"
+					  }
+					]
+				  })
+				return 
+			}
+		}
+	}
+	
+	if(!req.body.canBeDownloaded || typeof req.body.canBeDownloaded !== 'boolean') {
+		res.status(HTTP_STATUS.BAD_REQUEST_400).json({
+			errorsMessages: [
+			  {
+				message: "incorect iput value",
+				field: "canBeDownloaded"
+			  }
+			]
+		  })
+		return 
+	}
+
+	if(!req.body.minAgeRestriction || typeof req.body.minAgeRestriction !== 'number' || req.body.minAgeRestriction !== 18 ) {
+		res.status(HTTP_STATUS.BAD_REQUEST_400).json({
+			errorsMessages: [
+			  {
+				message: "incorect iput value",
+				field: "minAgeRestriction"
+			  }
+			]
+		  })
+		return 
+	}
+
+	if(!req.body.publicationDate || typeof req.body.publicationDate !== 'string' || req.body.publicationDate.length === 0 || req.body.publicationDate !== "2023-09-15T06:42:08.275Z" || !req.body.publicationDate.trim()) {
+
+		res.status(HTTP_STATUS.BAD_REQUEST_400).json({
+			errorsMessages: [
+			  {
+				message: "incorect iput value",
+				field: "publicationDate"
+			  }
+			]
+		  })
+		return 
+	}
 	
 	let foundVideos: any = videos.find(function(v) {
 		return v.id === Number(req.params.id)
 	})
-	// if(!foundVideos) {
-	// 	res.sendStatus(HTTP_STATUS.BAD_REQUEST_400)
-	// 	return
-	// }
+	if(!foundVideos) {
+		res.status(HTTP_STATUS.NOT_FOUND_404)
+		return
+	}
 
 	foundVideos.title = req.body.title
 	foundVideos.author = req.body.author
@@ -140,7 +234,7 @@ app.put('/videos/:id/', function(req: Request, res: Response) {
 	foundVideos.minAgeRestriction = req.body.minAgeRestriction
 	foundVideos.publicationDate = req.body.publicationDate
 
-	res.sendStatus(HTTP_STATUS.NO_CONTENT_204)
+	res.status(HTTP_STATUS.NO_CONTENT_204)
 })
 
 /******************************* DELETE{id} ****************************************/
